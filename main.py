ea83757bb5cec8da9f1ae7ff2727469fa4ea8eea
emasru <henning.s.strandaa@gmail.com>
2023-08-29 18:01:37 +0200

    Rewrote on_message logic into functions

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
-import discord
+from typing import Any, Union
+import discord as d
 from discord.ext import tasks
 
-class UndoButton(discord.ui.Button):
+VX_TWITTER_BASE_URL = "https://vxtwitter.com/"
+LINK_REPLACEMENTS = [
+    ("https://twitter", "https://vxtwitter"),
+    ("https://x", "https://vxtwitter"),
+    ("https://instagram", "https://ddinstagram")
+]
+
+class UndoButton(d.ui.Button):
     def __init__(self):
         super().__init__(
             label="↶",
-            style=discord.ButtonStyle.primary
+            style=d.ButtonStyle.primary
         )
     
     async def callback(self, interaction):
-        tweet_user = interaction.message.content.split("sent by: ", 1)[1]
-        if str(interaction.user) != tweet_user:
+        original_poster = interaction.message.content.split("sent by: ", 1)[1]
+        if str(interaction.user) != original_poster:
             return
 
-        reverted_content = interaction.message.content.replace('vxtwitter', 'twitter')
-        # Do it a second time for instagram, won't trigger if it doesnt find instagram in the link
-        reverted_content =  reverted_content.replace('ddinstagram', 'instagram')
-        
         delete = DeleteButton()
-        await interaction.message.edit(content=reverted_content, view=discord.ui.View(delete, timeout=None))
+        reverted_content = ""
+        for replacement_pair in LINK_REPLACEMENTS:
+            if replacement_pair[1] in interaction.message.content:
+                reverted_content = interaction.message.content.replace(replacement_pair[1], replacement_pair[0])
+                await interaction.message.content.edit(content=reverted_content, view=d.ui.View(delete, timeout=None))
+                break
+        
+    
         
         self.disabled = True
-        await interaction.response.defer()
+        await interaction.response.defer() # TODO
 
-class DeleteButton(discord.ui.Button):
+class DeleteButton(d.ui.Button):
     def __init__(self):
         super().__init__(
             label="❌",
-            style=discord.ButtonStyle.danger
+            style=d.ButtonStyle.danger
         )
     
-    async def callback(self, interaction: discord.Interaction):
+    async def callback(self, interaction: d.Interaction):
         tweet_user = interaction.message.content.split("sent by: ", 1)[1]
         if str(interaction.user) != tweet_user:
             return
@@ -40,48 +51,76 @@ class DeleteButton(discord.ui.Button):
 
 
 # Could be simplified
-bot = discord.Bot(intents=discord.Intents.all())
+bot = d.Bot(intents=d.Intents.all())
 
 def get_file_contents(file):
     return open(file).read()
 
+def reply_to_post(message: d.Message) -> bool:
+    # Checks if it even is a reply
+    if message.reference is None:
+        return False
+    
+    # Checks for if it is a system message
+    if message.is_system():
+        return False
+
+    # Checks if the original message is deleted
+    reply_unknown: Union[None, d.DeletedReferencedMessage, d.Message] = message.reference.resolved
+    if (not reply_unknown) or (reply_unknown is d.DeletedReferencedMessage):
+        return False
+
+    # Checks if user is in the server. "User" is reserved for users that are not in a Guild
+    mentioned_user: Union[d.User, d.Member] = reply_unknown.author # type: ignore TODO
+    if mentioned_user is d.User:
+        return False
+    
+    # Checks if the user replied to is the bot
+    if str(mentioned_user) != str(bot.user):
+        return False
+    
+    # Checks for a replaced link in the message, so that it actually is an embed
+    not_found = True
+    for replacement_pair in LINK_REPLACEMENTS:
+        if replacement_pair[1] in reply_unknown.content:
+            not_found = False
+            break
+    if not_found:
+        return False
+    
+    return True
+
+    
+async def link_replace(message: d.Message) -> None:
+    reply_content: str = ""
+    for replacement_pair in LINK_REPLACEMENTS:
+        if replacement_pair[0] in message.content:
+            reply_content = message.content.replace(replacement_pair[0], replacement_pair[1]) + " sent by: " + str(message.author)
+            break
+
+    if reply_content == "":
+        return
+
+    undo = UndoButton()
+    delete = DeleteButton()
+    reply_view = d.ui.View(undo, delete, timeout=None)
+
+    # await message.channel.send(content=reply_content, view=reply_view)
+    print("triggered sending message")
+    await message.delete()
+
 @bot.listen()
-async def on_message(message):
-    # Checks if reply and if bot is author of referenced message, and that its not author replying
-    # Purpose of this is to @ when replied to tweet/instagram post
-   if message.reference is not None and not message.is_system():
-        if (str(message.reference.resolved.author) == str(bot.user)):
-            # Check if reply is actually setup to mention the user
-            if not message.mentions:
-                return
-            author_str = message.reference.resolved.content.split("sent by: ", 1)[1]
-            if author_str == str(message.author):
-                return
-            username = author_str.split("#")
-            user = discord.utils.get(bot.users, name=username[0], discriminator=username[1])
-            if user is None:
-                return
-            else:
-                await message.channel.send(user.mention)
-   # Replaces the link and deletes the message
-   else:
-        if (message.content.find('https://twitter') != -1) and (message.content.find('https://vxtwitter') == -1):
-            reply_content = message.content.replace('twitter', 'vxtwitter') + " sent by: " + str(message.author)
-            undo = UndoButton()
-            delete = DeleteButton()
-            reply_view = discord.ui.View(undo, delete, timeout=None)
+async def on_message(message: d.Message):
+    if reply_to_post(message):
 
-            reply_message = await message.channel.send(content=reply_content, view=reply_view)
-            await message.delete()
-        # Normally would work without www, but instagram for some reason still uses www when copying in the browser
-        if (message.content.find('https://www.instagram') != -1) and (message.content.find('https://ddinstagram') == -1):
-            reply_content = message.content.replace('instagram', 'ddinstagram') + " sent by: " + str(message.author)
-            undo = UndoButton()
-            delete = DeleteButton()
-            reply_view = discord.ui.View(undo, delete, timeout=None)
+        author_str = message.reference.resolved.content.split("sent by: ", 1)[1]
+        user_tuple = (author_str.split("#")[0], author_str.split("#")[1])
+        user = d.utils.get(bot.users, name=user_tuple[0], discriminator=user_tuple[1]) # type: ignore
+        
+        await message.channel.send(user.mention)
+    
 
-            reply_message = await message.channel.send(content=reply_content, view=reply_view)
-            await message.delete()
+    await link_replace(message)
 
 
 if __name__ == "__main__":
