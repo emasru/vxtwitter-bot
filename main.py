dfae8ca2bdef1b486d535f92cec76c3fc053a682
emasru <henning.s.strandaa@gmail.com>
2023-08-29 18:12:49 +0200

    Typing fixes

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from typing import Any, Union
 import discord as d
 from discord.ext import tasks
 
+# TODO a few type ignores
+# TODO embed view expiry
+
 VX_TWITTER_BASE_URL = "https://vxtwitter.com/"
 LINK_REPLACEMENTS = [
     ("https://twitter", "https://vxtwitter"),
     ("https://x", "https://vxtwitter"),
     ("https://instagram", "https://ddinstagram")
 ]
+
+
+# Could be simplified
+bot = d.Bot(intents=d.Intents.all())
+
 
 class UndoButton(d.ui.Button):
     def __init__(self):
@@ -17,6 +25,9 @@ class UndoButton(d.ui.Button):
         )
     
     async def callback(self, interaction):
+        if not interaction.message:
+            return
+        
         original_poster = interaction.message.content.split("sent by: ", 1)[1]
         if str(interaction.user) != original_poster:
             return
@@ -26,7 +37,7 @@ class UndoButton(d.ui.Button):
         for replacement_pair in LINK_REPLACEMENTS:
             if replacement_pair[1] in interaction.message.content:
                 reverted_content = interaction.message.content.replace(replacement_pair[1], replacement_pair[0])
-                await interaction.message.content.edit(content=reverted_content, view=d.ui.View(delete, timeout=None))
+                await interaction.message.edit(content=reverted_content, view=d.ui.View(delete, timeout=None))
                 break
         
     
@@ -41,17 +52,17 @@ class DeleteButton(d.ui.Button):
             style=d.ButtonStyle.danger
         )
     
-    async def callback(self, interaction: d.Interaction):
-        tweet_user = interaction.message.content.split("sent by: ", 1)[1]
-        if str(interaction.user) != tweet_user:
+    async def callback(self, interaction):
+        if not interaction.message:
+            return
+        
+        original_poster = interaction.message.content.split("sent by: ", 1)[1]
+        if str(interaction.user) != original_poster:
             return
         
         await interaction.message.delete()
-        # await interaction.delete_original_message()
 
 
-# Could be simplified
-bot = d.Bot(intents=d.Intents.all())
 
 def get_file_contents(file):
     return open(file).read()
@@ -71,7 +82,7 @@ def reply_to_post(message: d.Message) -> bool:
         return False
 
     # Checks if user is in the server. "User" is reserved for users that are not in a Guild
-    mentioned_user: Union[d.User, d.Member] = reply_unknown.author # type: ignore TODO
+    mentioned_user: Union[d.User, d.Member] = reply_unknown.author # type: ignore
     if mentioned_user is d.User:
         return False
     
@@ -82,7 +93,7 @@ def reply_to_post(message: d.Message) -> bool:
     # Checks for a replaced link in the message, so that it actually is an embed
     not_found = True
     for replacement_pair in LINK_REPLACEMENTS:
-        if replacement_pair[1] in reply_unknown.content:
+        if replacement_pair[1] in reply_unknown.content: # type: ignore
             not_found = False
             break
     if not_found:
@@ -112,8 +123,10 @@ async def link_replace(message: d.Message) -> None:
 @bot.listen()
 async def on_message(message: d.Message):
     if reply_to_post(message):
+        if not message.reference or (message.reference is d.DeletedReferencedMessage):
+            return
 
-        author_str = message.reference.resolved.content.split("sent by: ", 1)[1]
+        author_str = message.reference.resolved.content.split("sent by: ", 1)[1] # type: ignore
         user_tuple = (author_str.split("#")[0], author_str.split("#")[1])
         user = d.utils.get(bot.users, name=user_tuple[0], discriminator=user_tuple[1]) # type: ignore
         
